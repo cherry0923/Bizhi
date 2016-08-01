e65fa5042cfefea7792703618111a7775d6100f7
cherry <cherry_zimo@163.com>
2016-08-01 22:06:27 +0800

    Initial Commit

---
+++ BiZhi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BiZhi
+//
+//  Created by Breeze on 16/8/1.
+//  Copyright © 2016年 Cz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
